1272ca5467d0367c0ba0992a23e5ec86a95db277
Albert <albert_triay@hotmail.com>
2021-09-27 00:10:46 +0200

    Dockerfile

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,7 @@
-[config]
-process.env.NODE_ENV = 'development'
-const environment = require('./environment')
-
-module.exports = environment.toWebpackConfig()
+plugins: [
+    new webpack.DefinePlugin({
+        "process.env": {
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production') // default value if not specified
+        }
+    })
+]
