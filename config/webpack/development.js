0b6421158d09326ee307ea8c6a16fd2960a18cfe
Albert <albert_triay@hotmail.com>
2021-09-27 00:11:14 +0200

    Dockerfile

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,7 +1,7 @@
 plugins: [
     new webpack.DefinePlugin({
         "process.env": {
-            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production') // default value if not specified
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') // default value if not specified
         }
     })
 ]
