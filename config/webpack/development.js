f6697ee974ee0d8f720f80a0d91856a1f6dd00d6
Albert <albert_triay@hotmail.com>
2021-10-03 16:12:03 +0200

    Dockerfile

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,11 +3,3 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-const environment = require('./environment')
-const path = require('path')
-const { config: webpackerConfig } = require('@rails/webpacker')
-
-environment.config.devtool = 'cheap-eval-source-map'
-environment.config.output.filename = '[name].js'
-module.exports = environment.toWebpackConfig()
